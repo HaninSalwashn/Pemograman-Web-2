eddd4c547ba78faf88e555c939b395516fc179ad
HaninSalwashn <115047063+HaninSalwashn@users.noreply.github.com>
2023-02-23 15:11:43 +0700

    pertemuan 1

---
+++ pertemuan1/tipe_data.php
@@ -0,0 +1,21 @@
+<?php
+// String 
+$name = 'Hanin';
+
+// Integer 
+$umur = 20;
+
+// Float
+$ipk = 3.9;
+
+// Boolean
+$isStudent = true;
+
+// Array dibahas terpisah 
+
+// menampilkan tipedata dan nilainya
+var_dump($name);
+
+
+echo '<br> Nama saya ' . $name . ' Umur saya adalah ' . $umur;
+echo "<br> Nama saya $name Umur saya adalah $umur";
---
+++ pertemuan1/variabel.php
@@ -0,0 +1,4 @@
+<?php
+$name = "Hanin Salwa";
+
+echo 'Hai, nama saya ' . $name;
